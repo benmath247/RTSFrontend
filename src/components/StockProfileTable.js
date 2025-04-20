4aff88dfedc2b72a3d477ceb34327bde4152c984
Ben Math <benjamin.aaron.math@gmail.com>
2025-04-20 14:40:01 -0400

    make table responsive

--- src/components/StockProfileTable.js
+++ src/components/StockProfileTable.js
@@ -35,38 +35,40 @@ const StockProfileTable = ({ stockProfileData }) => {
         </Card.Title>
         <Image src={logo} alt={`${name} logo`} height="50" className="mb-3" />
         <Card.Text>{description}</Card.Text>
-        <Table striped bordered hover>
-          <tbody>
-            <tr>
-              <th>Exchange</th>
-              <td>{exchange}</td>
-            </tr>
-            <tr>
-              <th>Industry</th>
-              <td>{industry}</td>
-            </tr>
-            <tr>
-              <th>Market Cap</th>
-              <td>
-                {marketCapitalization !== undefined
-                  ? `$${marketCapitalization.toLocaleString()}M`
-                  : "N/A"}
-              </td>
-            </tr>
-            <tr>
-              <th>Employees</th>
-              <td>{employeeTotal || "N/A"}</td>
-            </tr>
-            <tr>
-              <th>Website</th>
-              <td>
-                <a href={weburl} target="_blank" rel="noopener noreferrer">
-                  {weburl}
-                </a>
-              </td>
-            </tr>
-          </tbody>
-        </Table>
+        <div className="table-responsive">
+          <Table striped bordered hover>
+            <tbody>
+              <tr>
+                <th>Exchange</th>
+                <td>{exchange}</td>
+              </tr>
+              <tr>
+                <th>Industry</th>
+                <td>{industry}</td>
+              </tr>
+              <tr>
+                <th>Market Cap</th>
+                <td>
+                  {marketCapitalization !== undefined
+                    ? `$${marketCapitalization.toLocaleString()}M`
+                    : "N/A"}
+                </td>
+              </tr>
+              <tr>
+                <th>Employees</th>
+                <td>{employeeTotal || "N/A"}</td>
+              </tr>
+              <tr>
+                <th>Website</th>
+                <td>
+                  <a href={weburl} target="_blank" rel="noopener noreferrer">
+                    {weburl}
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
       </Card.Body>
     </Card>
   );
